55563af1b5c1c9fdf357a01e3b227fd7ffeb5e04
HAshIRa7 <maslennikov.em@phystech.edu>
2021-12-11 14:06:13 +0300

    inotify

--- inotify.c
+++ inotify.c
@@ -1,9 +1,7 @@
-#define _GNU_SOURCE
 #include <stdio.h> 
 #include <sys/stat.h> 
 #include <sys/types.h>
 #include <stdint.h>
-#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>   
@@ -11,35 +9,16 @@
 #include <signal.h> 
 #include <errno.h>
 #include <sys/inotify.h> 
-#include <sys/syscall.h>
 #include <fcntl.h> 
-#include <limits.h>
-#define LEN (sizeof(struct inotify_event) + NAME_MAX + 1)  
-#define TIME "%a %Y-%m-%d %H:%M:%S"
+#include <limits.h> 
 
-/*struct inotify_event {
-               int      wd;       // Watch descriptor 
-               uint32_t mask;     // Mask describing event 
-               uint32_t cookie;   // Unique cookie associating related
-                                  // events (for rename(2)) 
-               uint32_t len;      // Size of name field 
-               char     name[];   // Optional null-terminated name 
-           };
-*/
 
-extern int errno;  
-
-char* support = NULL;
-
-void handler(int sig) {
-    free(support);
-    exit(0);  
-}  
+void handler(int signo , siginfo_t *info , void *context) {
+    exit(0);
+}
 
 int HandleEvents(int dir_fd, int inotify_fd); 
-ssize_t ReadEvent(int inotify_fd, char* buf, size_t *buf_size); 
 static void displayInotifyEvent(struct inotify_event *event, int dir_fd); 
-static void PrintTime(struct statx_timestamp *stx_btime, char* name);
 
 int main(int argc, char** argv) {  
     
@@ -50,9 +29,10 @@ int main(int argc, char** argv) {
 
     int inotify_fd, wd;
 
-    struct sigaction sa = {}; 
-    sa.sa_handler = &handler; 
+    struct sigaction sa; 
     sigemptyset(&sa.sa_mask); 
+    sa.sa_sigaction = handler; 
+    sa.sa_flags = SA_RESTART | SA_SIGINFO; 
     if(sigaction(SIGINT, &sa, NULL) < 0) {
         perror("sigaction\n"); 
         return 2;
@@ -70,28 +50,24 @@ int main(int argc, char** argv) {
         return 4;
     } 
 
-    wd = inotify_add_watch(inotify_fd, argv[1], IN_CREATE);
+    wd = inotify_add_watch(inotify_fd, argv[1], IN_CREATE | IN_MOVE);
     if(wd < 0) {
         perror("inotify_add_watch\n"); 
         return 5; 
-    } 
-     
+    }
+
     int code = HandleEvents(dir_fd, inotify_fd); 
     return 0;
 } 
 
 int HandleEvents(int dir_fd, int inotify_fd) { 
-    size_t buf_size = LEN; 
-    char* buf = malloc(LEN);  
-    support = buf;
-    if(buf == NULL) {
-        perror("malloc"); 
-    }  
     struct inotify_event *event; 
     char* p; 
-    ssize_t NumRead; 
-    for(;;) {
-        NumRead = ReadEvent(inotify_fd, buf, &buf_size);  
+    ssize_t NumRead;  
+    size_t buf_size = sizeof(struct inotify_event) + NAME_MAX + 1;
+    for(;;) { 
+        char buf[buf_size];
+        NumRead = read(inotify_fd, buf, buf_size);  
         if(NumRead < 0) {
             printf("ReadEvent went wrong!\n");
             return 6;
@@ -102,48 +78,14 @@ int HandleEvents(int dir_fd, int inotify_fd) {
             p += sizeof(struct inotify_event) + event->len; 
         }
     }
-} 
-
-ssize_t ReadEvent(int inotify_fd, char* buf, size_t *buf_size) {
-    ssize_t result = read(inotify_fd, buf, *buf_size); 
-    if(result < 0) {
-        if(errno == EINVAL) {
-            *buf_size *= 2;
-            buf = realloc(buf, *buf_size);  
-            if(buf == NULL) {
-                free(buf);
-                return -1;
-            }
-            result = read(inotify_fd, buf, *buf_size); 
-            if(result < 0) {
-                perror("read\n"); 
-            }
-        } else {
-            perror("read"); 
-            free(buf);
-            return -1; 
-        }
-    } 
-    return result;
-} 
+}
 
 static void displayInotifyEvent(struct inotify_event *event, int dir_fd) {  
-    if(event->mask == IN_CREATE) {
-        struct statx statbuf; 
-        if(syscall(SYS_statx, dir_fd, event->name, AT_SYMLINK_NOFOLLOW, STATX_BTIME, &statbuf)); 
-        PrintTime(&statbuf.stx_btime, event->name);
+    if((event->mask & IN_CREATE) | (event->mask & IN_MOVED_TO)) {  
+        if(event->cookie > 0) {
+            printf("file was renamed to ");
+        }
+        printf("%s\n", event-> name);  
     }
     return;
-} 
-
-static void PrintTime(struct statx_timestamp *stx_btime, char* name) { 
-    struct tm tm; 
-    time_t btime; 
-    char time_buf[100]; 
-    
-    btime = stx_btime->tv_sec; 
-    tm = *localtime(&btime); 
-    strftime(time_buf, sizeof(time_buf), TIME, &tm); 
-    printf("[%s] File %s was created\n", time_buf, name);
-    return; 
 }
