5d1d13175de2d1293f5b300c06aad97d239aa002
Egor <90241890+HAshIRa7@users.noreply.github.com>
2021-12-16 22:52:35 +0300

    Delete str.c

--- 17_task/str.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h> 
-#include <string.h> 
-
-int main(int argc, char** argv) { 
-    char str[256]; 
-    snprintf(str, sizeof(argv[1]), "/%s", argv[1]); 
-    printf("%s\n", str);
-    return 0;
-}
