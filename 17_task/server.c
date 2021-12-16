78da5be49a4ad1b7cf8b7172076ac05cc1df5a01
HAshIRa7 <maslennikov.em@phystech.edu>
2021-12-17 00:21:32 +0300

    Client- server

--- 17_task/server.c
+++ 17_task/server.c
@@ -9,19 +9,23 @@
 
 void handler(int signum) {}
 
-int main(int argc, char** argv) {    
+int main(int argc, char** argv) {     
+    if(argc != 2) {
+        printf("Usage: %s open server", argv[0]);
+    }
+
     struct sigaction sa; 
     sigemptyset(&sa.sa_mask); 
     sa.sa_handler = handler;
-    sa.sa_flags = SA_RESTART; 
+    sa.sa_flags = 0; 
     if(sigaction(SIGINT, &sa, NULL) < 0) {
         perror("sigaction"); 
         return 2; 
     }
 
-    char* queue_name = argv[1];
-    mqd_t queue_fd = mq_open(queue_name, O_CREAT | O_EXCL | O_RDONLY, 0666, NULL); 
-    if(queue_fd == (mqd_t) - 1) {
+    const char* queue_name = argv[1];
+    mqd_t queue_fd = mq_open(queue_name, O_CREAT | O_EXCL | O_RDONLY, 0622, NULL); // писать могут все а читать только владелец
+    if(queue_fd == (mqd_t)-1) {
         perror("mq_open");
         return 3;  
     }   
@@ -40,26 +44,24 @@ int main(int argc, char** argv) {
     ssize_t read_bytes = 0; 
     unsigned int prio = 0;  
     while(1) { 
-        read_bytes = mq_receive(queue_fd, buffer, msg_size, &prio);   
+        read_bytes = mq_receive(queue_fd, buffer, msg_size, &prio); 
         if(read_bytes < 0) {
-            if(errno == EINVAL) {
-                free(buffer); 
-                if(mq_close(queue_fd) < 0) {
-                    perror("mq_close");
-                    return 6;
-                } 
-                if(mq_unlink(queue_name) < 0) {
-                    perror("mq_inlink");
-                    return 7;
-                }
-                break;
-            } else {
-                perror("mq_receive");
-                return 8; 
-            }
-        } 
+            break;
+        }  
         buffer[read_bytes] = '\0';
         printf("%s\n", buffer);
+    }  
+    if(errno != EINTR && errno != EAGAIN) {
+        perror("mq_receive"); 
+    }
+    free(buffer); 
+    if(mq_close(queue_fd) < 0) {
+        perror("mq_close");
+        return 6;
+    } 
+    if(mq_unlink(queue_name) < 0) {
+        perror("mq_inlink");
+        return 7;
     }
     return 0; 
 }
